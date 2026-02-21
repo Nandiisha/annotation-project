ea50a28df655cd03fc7cee2c336f2014246fc23b
Isha Nandi <ishanandi2022@vitbhopal.ac.in>
2026-02-22 00:16:32 +0530

    fix cors for vercel

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,13 @@ require("dotenv").config();
 const app = express();
 
 /* ================= MIDDLEWARE ================= */
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+  })
+);
 app.use(express.json({ limit: "200mb" }));
 app.use(express.urlencoded({ limit: "200mb", extended: true }));
 
